fe32a503d7661a2ec61c5484221b955234595fab
Ian Vlahovic <banddorkfht7@gmail.com>
2018-01-26 03:24:03 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
+# Given an argument of a country name,
+# it iterates over a hash where {animal names => country names}
+# and returns an array containing animals from that country
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    map { |key, value| arguments.include?(value) ? key : nil }.compact
   end
 end
